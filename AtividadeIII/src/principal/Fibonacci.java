285883a419d42c251e0a9ae975a8962d173ebb70
SamuelSinfronio <samuelsinfronio@gmail.com>
2025-10-17 09:42:53 -0300

    Update Fibonacci.java

--- AtividadeIII/src/principal/Fibonacci.java
+++ AtividadeIII/src/principal/Fibonacci.java
@@ -1,5 +1,19 @@
 package principal;
 
 public class Fibonacci {
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Série de Fibonacci até ultrapassar 100:");
+        int i = 0, valor;
+        do {
+            valor = fib(i);
+            System.out.print(valor + " ");
+            i++;
+        } while (valor <= 100);
+    }
 }
+
