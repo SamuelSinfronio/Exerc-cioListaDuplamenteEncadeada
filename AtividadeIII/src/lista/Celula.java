31c0608bfbe923cc552ecbaab68d022172f956f1
SamuelSinfronio <samuelsinfronio@gmail.com>
2025-10-17 09:39:48 -0300

    Implement Celula class with Contato reference

--- AtividadeIII/src/lista/Celula.java
+++ AtividadeIII/src/lista/Celula.java
@@ -1,5 +1,38 @@
 package lista;
 
-public class Celula {
+import basicas.Contato;
 
+public class Celula {
+    private Contato contato;
+    private Celula anterior;
+    private Celula proximo;
+
+    public Celula(Contato contato) {
+        this.contato = contato;
+        this.anterior = null;
+        this.proximo = null;
+    }
+
+    public Contato getContato() {
+        return contato;
+    }
+
+    public Celula getAnterior() {
+        return anterior;
+    }
+
+    public Celula getProximo() {
+        return proximo;
+    }
+
+    public void setAnterior(Celula anterior) {
+        this.anterior = anterior;
+    }
+
+    public void setProximo(Celula proximo) {
+        this.proximo = proximo;
+    }
 }
+
+
+
