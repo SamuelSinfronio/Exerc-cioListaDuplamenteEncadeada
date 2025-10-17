a6d1e91e83633fc71af409f2ce948a582ddb6dff
SamuelSinfronio <samuelsinfronio@gmail.com>
2025-10-17 09:42:05 -0300

    Update BinarioDecimal.java

--- AtividadeIII/src/principal/BinarioDecimal.java
+++ AtividadeIII/src/principal/BinarioDecimal.java
@@ -1,5 +1,17 @@
 package principal;
 
 public class BinarioDecimal {
+    public static int converter(String binario, int indice) {
+        if (indice == binario.length()) return 0;
 
+        int bit = binario.charAt(binario.length() - 1 - indice) - '0';
+        return bit * (int)Math.pow(2, indice) + converter(binario, indice + 1);
+    }
+
+    public static void main(String[] args) {
+        String binario = "11001"; // exemplo: 11001 = 25
+        int decimal = converter(binario, 0);
+        System.out.println("Binário: " + binario + " → Decimal: " + decimal);
+    }
 }
+
