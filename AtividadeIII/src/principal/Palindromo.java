dafbf27157ce1985b1b233a6725ad33a14bb4dab
SamuelSinfronio <samuelsinfronio@gmail.com>
2025-10-17 09:43:48 -0300

    Add palindrome checking functionality
    Implement palindrome checker using a stack.

--- AtividadeIII/src/principal/Palindromo.java
+++ AtividadeIII/src/principal/Palindromo.java
@@ -1,5 +1,31 @@
 package principal;
 
-public class Palindromo {
+import java.util.Stack;
+import java.util.Scanner;
 
+public class Palindromo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite uma palavra ou frase: ");
+        String texto = sc.nextLine().replaceAll("\\s+", "").toLowerCase();
+
+        Stack<Character> pilha = new Stack<>();
+
+        for (char c : texto.toCharArray()) {
+            pilha.push(c);
+        }
+
+        String invertido = "";
+        while (!pilha.isEmpty()) {
+            invertido += pilha.pop();
+        }
+
+        if (texto.equals(invertido))
+            System.out.println("É um palíndromo!");
+        else
+            System.out.println("Não é um palíndromo.");
+
+        sc.close();
+    }
 }
+
